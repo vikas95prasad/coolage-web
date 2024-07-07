36efc17e5c0a2946c8fc25081b4da2161bd25cd0
Vikas Prasad <vprasad8@apple.com>
2024-07-07 23:06:29 +0530

    Update APIs root URL

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+const DEV_URL = 'http://localhost:3000/';
+const PROD_URL = 'https://coolage-5fbe878e57bb.herokuapp.com/';
+const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
+
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/graphql', // Replace with your GraphQL endpoint
+  uri: `${API_ROOT}/graphql`
 });
 
 const authLink = setContext((_, { headers }) => {
