40f81a4dce3de3d3b8dd1e61129a34776e1253dc
Vikas Prasad <vprasad8@apple.com>
2024-07-07 13:10:08 +0530

    Updated Menu UI

---
+++ src/components/MenuSidebar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const MenuSidebar: React.FC<{ menus: any[] }> = ({ menus }) => {
+  return (
+    <div className='fixed'>
+      <div className='hidden lg:block'>
+        <div className="flex flex-col flex-grow bg-default lg:bg-transparent transition-all lg:overflow-y-auto pt-6 sm:pt-12 pb-4">
+          <div className='flex flex-col flex-grow'>
+            <nav className='relative flex-1'>
+              <div className='lg:mb-5 transition-all ease-out text-3xl font-display text-center lg:text-left opacity-100'>Our Menu</div>
+              <div className='no-scrollbar sm:ml-0 -ml-3 sm:mr-0 -mr-3'>
+                <div className='flex justify-center min-w-max lg:min-w-0 lg:block'>
+                {menus.map((menu) => (
+                  <div className='relative justify-center flex-none p-2 lg:p-0 lg:w-full align-center'>
+                    <a className='border-b-2 lg:border-b-0 border-primary text-default font-bold hover:text-default group w-full lg:border-l-2 lg:flex items-center text-center lg:text-left p-2 text-sm font-medium focus:color-primary'>{menu.label}</a>
+                  </div>))}
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuSidebar;
